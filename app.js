de897d88319e7bac62e6aa4599756c7471c7f263
skhokhlov <worldfly@yandex.com>
2014-04-10 19:06:08 +0400

    add HTTP 200

--- app.js
+++ app.js
@@ -22,8 +22,7 @@ app.get('/', function(req, res){
     if(req.host == 'worldfly.info' || req.host == 'www.worldfly.info' || req.host == 'worldfly.org'){
         res.redirect(301, 'http://www.worldfly.org');
     } else{
-    res.status(200);
-    res.sendfile('index.html');
+    res.status(200).sendfile('index.html');
     }
 });
 

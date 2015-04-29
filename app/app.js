f23eebe4a149a4e60419cd5429ac0be57defea36
skhokhlov <worldfly@yandex.ru>
2015-04-29 18:09:54 +0300

    Improving documentation

--- app/app.js
+++ app/app.js
@@ -1,16 +1,11 @@
 (function (yr, window, document) {
 
     var app = {
-        launch: function () {
-            if (window.location.pathname === '/') {
-                history.replaceState({page: 'home'}, 'Home page of World Fly', '/');
-                app.pages.home();
-            } else if (window.location.pathname === '/projects') {
-                history.replaceState({page: 'projects'}, 'Projects of World Fly', '/projects');
-                app.pages.projects();
-            }
-        },
+        /**
+         * Enabling cross page navigation
+         */
         navigation: function () {
+            //Checking for b-nav__link_home element on page. For example on homepage this element is undefined.
             if (document.getElementsByClassName('b-nav__link_home')[0] !== undefined) {
                 document.getElementsByClassName('b-nav__link_home')[0].addEventListener('click', function (event) {
                     history.pushState({page: 'home'}, 'Home page of World Fly', '/');
@@ -28,11 +23,17 @@
             });
         },
         pages: {
+            /**
+             * Actions for activating homepage
+             */
             home: function () {
                 document.title = 'Home page of World Fly';
                 document.getElementsByTagName('html')[0].innerHTML = app.render.home;
                 app.navigation();
             },
+            /**
+             * Actions for activating projects page
+             */
             projects: function () {
                 document.title = 'Projects of World Fly';
                 document.getElementsByTagName('html')[0].innerHTML = app.render.projects;
@@ -45,6 +46,9 @@
         }
     };
 
+    /**
+     * Actions for back and forward buttons navigation
+     */
     window.addEventListener('popstate', function (event) {
         if (event.state.page === 'home') {
             app.pages.home();
@@ -53,6 +57,12 @@
         }
     });
 
-    app.launch();
+    if (window.location.pathname === '/') {
+        history.replaceState({page: 'home'}, 'Home page of World Fly', '/');
+        app.pages.home();
+    } else if (window.location.pathname === '/projects') {
+        history.replaceState({page: 'projects'}, 'Projects of World Fly', '/projects');
+        app.pages.projects();
+    }
 
 })(yr, window, document);
--- public/bootstrap.js
+++ public/bootstrap.js
@@ -1,96 +1,115 @@
 (function Bootstrap(window) {
 
     /**
-    * Browser tests
-    * If client don't support Blob URLs or JSON he wiil be redirected to version without js
-    */
+     * Browser tests
+     * If client don't support Blob URLs or JSON he wiil be redirected to version without js
+     */
     if (!new Blob() && !('JSON' in window && 'parse' in JSON && 'stringify' in JSON)) {
-      window.location = window.location.href + '?nojs=true';
+        window.location = window.location.href + '?nojs=true';
     }
 
+    /**
+     * Make XHR GET to url
+     * @param url
+     * @returns {Promise}
+     */
     var request = function (url) {
-        return new Promise(function (resolve, reject) {
-            var request = new XMLHttpRequest();
-            request.open('GET', url, true);
-            request.onload = function () {
-                if (request.status === 200) {
-                    resolve(request.response);
-                } else {
-                    reject(Error(request.statusText));
-                }
-            };
-            request.onerror = function () {
-                reject(Error("Network Error"));
-            };
-            request.send(null);
-        });
-    },
+            return new Promise(function (resolve, reject) {
+                var request = new XMLHttpRequest();
+                request.open('GET', url, true);
+                request.onload = function () {
+                    if (request.status === 200) {
+                        resolve(request.response);
+                    } else {
+                        reject(Error(request.statusText));
+                    }
+                };
+                request.onerror = function () {
+                    reject(Error("Network Error"));
+                };
+                request.send(null);
+            });
+        },
+        /**
+         * Construct Blob with current params and append it to document.head
+         * @param content
+         * @param type
+         * @param id
+         * @constructor
+         */
+        MyBlob = function (content, type, id) {
+            window.URL = window.URL || window.webkitURL;
 
-    MyBlob = function (content, type, id) {
-        window.URL = window.URL || window.webkitURL;
+            var blob = new Blob(content, {type: type});
 
-        var blob = new Blob(content, {type: type});
+            if (type === 'text/css') {
+                var link = document.createElement('link');
+                link.id = id;
+                link.rel = 'stylesheet';
+                link.href = window.URL.createObjectURL(blob);
+                document.head.appendChild(link);
+            } else if (type === 'text/javascript') {
+                var script = document.createElement('script');
+                script.id = id;
+                script.src = window.URL.createObjectURL(blob);
+                document.head.appendChild(script);
+            }
+        },
 
-        if (type === 'text/css') {
-            var link = document.createElement('link');
-            link.id = id;
-            link.rel = 'stylesheet';
-            link.href = window.URL.createObjectURL(blob);
-            document.head.appendChild(link);
-        } else if (type === 'text/javascript') {
-            var script = document.createElement('script');
-            script.id = id;
-            script.src = window.URL.createObjectURL(blob);
-            document.head.appendChild(script);
-        }
-    },
+        blobContent = {},
+        blobContentLoad = 0,
 
-    blobContent = {},
-    blobContentLoad = 0,
-
-    BlobRender = function () {
-        if (blobContentLoad === 4) {
-            MyBlob([blobContent.runtimejs, blobContent.appyatejs, blobContent.appjs], 'text/javascript', 'js');
-        }
-    },
-
-    BootstrapError = function (error) {
-        window.wf.BootCount++;
-        console.error(error);
-        if (window.wf.BootCount < 1) {
-            console.warn('Loading error. I\'m try again now');
-            Bootstrap();
-        }
-    };
+        /**
+         * Runs MyBlob function after loading everything
+         */
+        blobRender = function () {
+            if (blobContentLoad === 4) {
+                MyBlob([blobContent.runtimejs, blobContent.appyatejs, blobContent.appjs], 'text/javascript', 'js');
+            }
+        },
+        /**
+         * Runs actions for error bootstrapping
+         * @param error
+         */
+        bootstrapError = function (error) {
+            window.wf.BootCount++;
+            console.error(error);
+            if (window.wf.BootCount < 1) {
+                console.warn('Loading error. I\'m try again now');
+                Bootstrap();
+            }
+        };
 
     request('/public/runtime.js').then(function (res) {
         blobContent.runtimejs = res;
         blobContentLoad++;
-        BlobRender();
+        blobRender();
     }, function (error) {
-        BootstrapError(error);
+        bootstrapError(error);
     });
+
     request('/public/app.yate.js').then(function (res) {
         blobContent.appyatejs = res;
         blobContentLoad++;
-        BlobRender();
+        blobRender();
     }, function (error) {
-        BootstrapError(error);
+        bootstrapError(error);
     });
+
     request('/public/app.js').then(function (res) {
         blobContent.appjs = res;
         blobContentLoad++;
-        BlobRender();
+        blobRender();
     }, function (error) {
-        BootstrapError(error);
+        bootstrapError(error);
     });
 
     request('/assest/data.json').then(function (res) {
         window.wf.PagesData = res;
         blobContentLoad++;
-        BlobRender();
+        blobRender();
     }, function (error) {
-        BootstrapError(error);
+        bootstrapError(error);
     });
 
 })(window);

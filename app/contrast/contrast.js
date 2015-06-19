d3e34ad3fafc37735ea5c97849e69a210a5bbf3b
skhokhlov <worldfly@yandex.ru>
2015-06-19 11:07:19 +0300

    Add browser testing. Add copyright

--- app/contrast/contrast.js
+++ app/contrast/contrast.js
@@ -1,16 +1,26 @@
 (function () {
+
+    var el = document.getElementsByClassName('b-body__content')[0];
+
+    /**
+     * Browser tests
+     * If client don't support XMLHttpRequest or JSON he will be redirected to version without js
+     */
+    if (!new XMLHttpRequest() && !('JSON' in window && 'parse' in JSON && 'stringify' in JSON)) {
+        el.innerHTML = '<h2 class="b-error">Your browser isn\'t supported. Sorry...</h2>';
+    }
+
     var Request = new XMLHttpRequest();
     Request.open('GET', '/assest/photos.json', true);
     Request.onload = function () {
         if (Request.status === 200) {
             var r = JSON.parse(Request.responseText),
-                i = r.photo.length,
-                el = document.getElementsByClassName('b-body__content')[0];
+                l = r.photo.length;
 
-            while (i--) {
+            for (var i = 0; i < l; i++) {
                 var img = document.createElement('img');
-                img.src = r.photo[i].url;
-                img.class = 'b-list__item';
+                img.src = r.photo[i].url.replace('{size}', '');
+                img.className = 'b-list__item';
                 el.appendChild(img);
             }
 

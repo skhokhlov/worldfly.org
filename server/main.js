e855acebd69ad6d3de66a9178133d121722602b9
skhokhlov <worldfly@yandex.ru>
2015-07-02 18:34:09 +0300

    Optimization

--- server/main.js
+++ server/main.js
@@ -1,7 +1,8 @@
 module.exports = function (app) {
     var dirname = global.dirname,
         yr = require(dirname + '/node_modules/yate/lib/runtime.js'),
-        lib = require('./lib.js');
+        lib = require('./lib.js'),
+        dataAPI = require('./data.js');
 
 
     app.get('/', function (req, res) {
@@ -33,13 +34,13 @@ module.exports = function (app) {
     });
 
     app.get('/assest/data.json', function (req, res) {
-        res.status(200).set('cache-control', 'public, max-age=120').json(getData());
+        res.status(200).set('cache-control', 'public, max-age=120').json(dataAPI);
     });
 
     require(dirname + '/public/app.yate.js');
     var render = {
-        "home": yr.run('app', getData().home),
-        "projects": yr.run('app', getData().projects)
+        "home": yr.run('app', dataAPI.home),
+        "projects": yr.run('app', dataAPI.projects)
     };
 
     /**
@@ -59,176 +60,5 @@ module.exports = function (app) {
             });
         });
     }
-
-    function getFile(path) {
-        return require('fs').readFileSync(path, {encoding: 'utf-8'}, function (err, data) {
-            if (err) {
-                throw err;
-            }
-            return data;
-        });
-    }
-
-    function getData(env) {
-        var _data = {
-            "year": new Date().getFullYear(),
-            "nojs": (env === 'nojs' ? "true" : "false"),
-            "debug": process.env.DEBUG
-        };
-        return {
-            "home": {
-                "data": _data,
-                "page": {
-                    "page-blocks": {
-                        "header": {
-                            "logo": true,
-                            "nav": true
-                        },
-                        "footer": true
-                    },
-                    "page-params": {
-                        "_page": "home",
-                        "title": "Homepage of World Fly"
-                    }
-                }
-            },
-            "projects": {
-                "data": _data,
-                "page": {
-                    "page-blocks": {
-                        "header": {
-                            "logo": true,
-                            "nav": true
-                        },
-                        "body": {
-                            "projects": true
-                        },
-                        "footer": true
-                    },
-                    "page-params": {
-                        "_page": "page",
-                        "title": "Projects of World Fly",
-                        "header": "Projects"
-                    },
-                    "projects": {
-                        "list": {
-                            "project": [
-                                {
-                                    "title": "Contrast",
-                                    "id": "Contrast",
-                                    "link": [
-                                        {
-                                            "href": "/contrast",
-                                            "body": "Website",
-                                            "separator": ". "
-                                        }
-                                    ],
-                                    "description": getFile('projects/build/contrast.html')
-                                },
-                                {
-                                    "title": "Yamomof",
-                                    "id": "Yamomof",
-                                    "years": {
-                                        "begin": 2015
-                                    },
-                                    "link": [
-                                        {
-                                            "href": "https://github.com/skhokhlov/yamomof",
-                                            "body": "Github",
-                                            "separator": ". "
-                                        }
-                                    ],
-                                    "description": getFile('projects/build/yamomof.html')
-                                },
-                                {
-                                    "title": "WalletMap",
-                                    "id": "WalletMap",
-                                    "years": {
-                                        "begin": 2015
-                                    },
-                                    "description": getFile('projects/build/walletmap.html')
-                                },
-                                {
-                                    "title": "Gewefope",
-                                    "id": "Gewefope",
-                                    "years": {
-                                        "begin": 2013,
-                                        "end": "∞"
-                                    },
-                                    "link": [
-                                        {
-                                            "href": "https://github.com/gewefope/gewefope",
-                                            "body": "Github",
-                                            "separator": ". "
-                                        },
-                                        {
-                                            "href": "https://gewefope.worldfly.org",
-                                            "body": "Website",
-                                            "separator": "."
-                                        }
-                                    ],
-                                    "description": getFile('projects/build/gewefope.html')
-                                },
-                                {
-                                    "title": "Greengrocery",
-                                    "id": "Greengrocery",
-                                    "years": {
-                                        "begin": 2012,
-                                        "end": "∞"
-                                    },
-                                    "link": [
-                                        {
-                                            "href": "https://github.com/skhokhlov/greengrocery",
-                                            "body": "Github",
-                                            "separator": "."
-                                        }
-                                    ],
-                                    "description": getFile('projects/build/greengrocery.html')
-                                },
-                                {
-                                    "title": "Blog about IT",
-                                    "years": {
-                                        "begin": 2012
-                                    },
-                                    "description": getFile('projects/build/blog_about_it.html')
-                                },
-                                {
-                                    "title": "Collection of articles on aviation and astronautics",
-                                    "years": {
-                                        "begin": 2008,
-                                        "end": 2013
-                                    },
-                                    "description": getFile('projects/build/collection_of_articles_on_aviation_and_astronautics.html')
-                                },
-                                {
-                                    "title": "News of aviation and astronautics",
-                                    "years": {
-                                        "begin": 2008,
-                                        "end": 2012
-                                    },
-                                    "description": getFile('projects/build/news_of_aviation_and_astronautics.html')
-                                },
-                                {
-                                    "title": "Photo Album",
-                                    "years": {
-                                        "begin": 2008,
-                                        "end": 2013
-                                    },
-                                    "description": getFile('projects/build/photo_album.html')
-                                },
-                                {
-                                    "title": "Encyclopedia of aviation and astronautics",
-                                    "years": {
-                                        "begin": 2007,
-                                        "end": 2013
-                                    },
-                                    "description": getFile('projects/build/encyclopedia_of_aviation_and_astronautics.html')
-                                }
-                            ]
-                        }
-                    }
-                }
-            }
-        };
-    }
+    
 };
